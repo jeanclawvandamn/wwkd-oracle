162304a17abe855dcb832b8bc0eb624ae3c06477
JeanClaw VanDamn <jeanclawvandamn@gmail.com>
2026-02-06 18:36:42 -0800

    WWKD - What Would Kanye Do oracle

---
+++ server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    let filePath = req.url === '/' ? '/index.html' : req.url;
+    filePath = path.join(__dirname, filePath);
+    
+    const ext = path.extname(filePath);
+    const contentTypes = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'application/javascript',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg',
+        '.gif': 'image/gif',
+        '.ico': 'image/x-icon'
+    };
+    
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('Not found');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentTypes[ext] || 'text/html' });
+            res.end(content);
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`🌊 WWKD Oracle serving on port ${PORT}`);
+});
